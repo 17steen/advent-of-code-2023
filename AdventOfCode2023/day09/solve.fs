f7f09d2b07e129aa18929a5d945d15bb3847d685
17steen <robinoger1207@gmail.com>
2023-12-10 14:20:02 +0100

    day09

--- AdventOfCode2023/day09/solve.fs
+++ AdventOfCode2023/day09/solve.fs
@@ -5,27 +5,62 @@ open AdventOfCode2023
 open FSharp.Collections.ParallelSeq
 open FSharp.Text.RegexProvider
 
-
 let parse_line = 
-    Utils.space_split >> Array.map int
+    Utils.space_split >> Array.map int64
     
 let parse = Utils.lines >> Array.map parse_line
 
+let differences array =
+    Array.pairwise array
+    |> Array.map (fun (a, b) -> b - a)
 
-let solve (input: string) = 
+let rec extrapolate input  =
+    let differences = differences input
+    
+    
+    if Array.forall ((=) LanguagePrimitives.GenericZero) differences then
+        Array.last input
+    else 
+        let extrapolated = 
+            extrapolate differences
+        Array.last input + extrapolated 
+
+let rec extrapolate_back input  =
+
+    let differences = differences input
+    
+    if Array.forall ((=) LanguagePrimitives.GenericZero) differences then
+        Array.head input
+    else 
+        let extrapolated = 
+            extrapolate_back differences
+        Array.head input - extrapolated 
+        
+let example = 
+    """
+0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
+    """.Trim()
+    
+let test = 
+    
+    let first = parse example |> Array.head
+    
+    extrapolate first
+
+
+let pass_through func value = 
+    func value
+    value
+
+let solve (input: string) =
 
     let parsed = parse input
+    let part1 = parsed |> Array.map extrapolate |> Array.sum
+    let part2 = parsed |> Array.map extrapolate_back |> Array.sum
     
     
-    
-    // nodes |> Map.iter (fun key (left, right) -> printfn "%A -> %A %A" (unhash_id key) (unhash_id left) (unhash_id right))
-    
-    nodes 
-    |> Map.containsKey (hash_id "ZZZ")
-    |> printfn "%A"
-
-    let part1 = count_distance nodes "AAA" "ZZZ" directions
-    
-    (part1, 0)
+    (part1, part2)
 
 
--- Runner/Program.fs
+++ Runner/Program.fs
@@ -1,5 +1,6 @@
 ﻿
 let input = System.Console.In.ReadToEnd() |> _.Trim()
-let result = AdventOfCode2023.Days.Day08.solve input
+let result = AdventOfCode2023.Days.Day09.solve input
+
 
 printfn "%A" result
